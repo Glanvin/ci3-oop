8c8f9a75e779398e73772dc782beefe094c87fcd
Glavin <glavin.code@gmail.com>
2026-08-18 12:23:45 +0800

    Fix: Alert alignment

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,5 @@
 <div class="position-relative">
-  <div class="position-absolute top-0 start-100 translate-middle m-3" style="z-index: 10; max-width: 800px;">
+  <div class="position-absolute top-0 end-0  m-3" style="z-index: 10; max-width: 800px;">
     <?= $this->notify->render(); ?>
     </div>  
 </div>
